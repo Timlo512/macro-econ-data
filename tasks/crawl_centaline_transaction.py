c75cdfd439d8b324a6f71cb0d27c22a8e94fe64f
Timothy Lo <timothy@qupital.com>
2025-05-21 21:55:06 +0800

    Added crawl_centaline_transaction job

---
+++ tasks/crawl_centaline_transaction.py
@@ -0,0 +1,75 @@
+import requests
+import json, os
+import pandas as pd
+from src.PostgresConnector import PostgresConnector
+
+import dotenv
+dotenv.load_dotenv()
+from datetime import datetime
+
+# Define the URL and payload
+url = "https://hk.centanet.com/findproperty/api/Transaction/Search"
+payload = {
+    "amountRange": {},
+    "bigPhotoMode": False,
+    "day": "Day1095",
+    "offset": 0,
+    "order": "Descending",
+    "pageSource": "search",
+    "postType": "Sale",
+    "size": 24,
+    "sort": "InsOrRegDate"
+}
+
+# Add to the params
+search_keywords = [
+    "", "將軍澳廣場"
+]
+
+conn = PostgresConnector(
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT")
+)
+load_datetime = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+
+    search_keyword = search_keywords[1]
+    for search_keyword in search_keywords:
+        if search_keyword:
+            payload["keyword"] = search_keyword
+
+        # Make the POST request
+        response = requests.post(url, json=payload)
+
+        # Check the response status and print the result
+        if response.status_code == 200:
+            print("Request successful!")
+            data = response.json()
+
+            # Convert the data to a pandas DataFrame
+            data_list = data.get("data", [])
+            df = pd.DataFrame(data_list)
+
+            # convert all dict and list columns to string
+            for col in df.columns:
+                if isinstance(df[col].iloc[0], (dict, list)):
+                    df[col] = df[col].apply(lambda x: json.dumps(x, ensure_ascii=False))
+
+            df = df.rename({"id": "transactionId"}, axis = 1)
+            df["keyword"] = search_keyword
+            df["load_datetime"] = load_datetime
+
+            # Save the DataFrame to PostgreSQL
+            conn.write_data(df, "centaline_transaction", 
+                    if_exists="upsert", 
+                    conflict_columns=["transactionId"],
+                    index=False)
+            print(f"Data saved to table 'centaline_transaction'")
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            print(response.text)
